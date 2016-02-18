5139ebfd004315f45a216da081c43c35deb7b269
Ethan Jones <etmajo@gmail.com>
2016-02-18 18:10:58 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -1,1 +1,25 @@
-// State: Dodge.Play
+//State dodge.play
+
+'use strict'
+
+var dodge = {};
+
+dodge.play = function () {};
+
+dodge.play.prototype = {
+
+  inti: function () {
+    console.log("%c~~~ Booting Dodge ~~~/n Compliments of SkilStak", " color:#fdfe3; backround:073642)
+  },
+
+  preload function () {
+    this.load.image('backgroud', 'assets/space.png')
+  create: function () {
+    this.background = this.add.tileSprite(0,0,320,568,"space.png");
+    this.background.autoScroll(0,50);
+    this.background.scale.set(1);
+  },
+
+  update: function () {
+  }
+}
